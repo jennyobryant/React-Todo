9ceacb31ebf12d3603e1667857e7aa8b6649bf43
jenny obryant <jennyobryant@protonmail.com>
2019-10-03 11:51:33 -0600

    say hello

--- src/App.js
+++ src/App.js
@@ -1,98 +1,144 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-import TodoForm from "./components/TodoComponents/TodoForm"; 
-import TodoList from "./components/TodoComponents/TodoList"; 
+//import TodoForm from "./components/TodoComponents/TodoForm"; 
+//import TodoList from "./components/TodoComponents/TodoList"; 
 
-import Todo from "./components/TodoComponents/Todo"; 
+//import Todo from "./components/TodoComponents/Todo"; 
 
-class App extends React.Component {
+// data [
+//   {
+//       task: 'Organize Garage',
+//       id: 1528817077286,
+//       completed: false 
+//   },
+//   {
+//       task: 'Bake Cookies',
+//       id: 1528817084358,
+//       completed: false   
+//   }
+// ]; 
+
+// const [data, setDate] = useState({}); 
+// useEffect (()=> {
+//   axios.get()
+//   .then(res => {
+//     setData(res.data); 
+//   }); 
+// }, []); 
+
+// return (
+//   <div className = "App">
+//     <h1 className = "Header">Todo List: MVP</h1>
+//     <div className ="containter">
+//       {data.results && data.results.map(data => (
+//         <Todo data = {data}
+//         />
+//       ))}
+//     </div>
+
+//   </div>
+
+//   ) 
+
+function App(){
+return (
+<div>
+  Hello
+</div>
+
+); 
+}
+//class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-    constructor(){
-      super()
-      this.state ={
-        tasks: data
-      }
-    }
 
-  toggleItem = (event, itemId) => {
-    event.preventDefault()
 
-    this.setState({
-      tasks: this.state.tasks.map(item => {
-        if (item.id === itemId){
-          return {
-            ...item, 
+
+//     constructor(){
+//       super()
+//       this.state ={
+//         tasks: data
+//       }
+//     }
+
+//   toggleItem = (event, itemId) => {
+//     event.preventDefault()
+
+//     this.setState({
+//       tasks: this.state.tasks.map(item => {
+//         if (item.id === itemId){
+//           return {
+//             ...item, 
             
-            task: !item.task
+//             task: !item.task
             
-          }
-        } else {
+//           }
+//         } else {
 
-          return item
+//           return item
 
-        }
+//         }
 
           
-        })
+//         })
     
-    })
+//     })
 
-  }
+//   }
 
-  cleartask = (event) => {
-    event.preventDefault()
+//   cleartask = (event) => {
+//     event.preventDefault()
 
-    this.setState ({
-      tasks: this.state.tasks.filter(item => {
-        return !item.task
+//     this.setState ({
+//       tasks: this.state.tasks.filter(item => {
+//         return !item.task
 
-      })
-    })
-  }
+//       })
+//     })
+//   }
 
-  addTask = (event, taskName) => {
-    const newTask = {
-      id: Date.now(),
-      name: taskName,
-      task: false
-    }
+//   addTask = (event, taskName) => {
+//     const newTask = {
+//       id: Date.now(),
+//       name: taskName,
+//       task: false
+//     }
 
-    this.setState({
-      tasks: [newTask, ...this.state.tasks]
-    })
-  }
+//     this.setState({
+//       tasks: [newTask, ...this.state.tasks]
+//     })
+//   }
 
-  render() {
-    return (
-      <div className = "App">
-        <div className ="header">
-        <h2>Todo List: MVP </h2>
-        <TodoForm addTask ={this.addTask} />
+//   render() {
+//     return (
+//       <div className = "App">
+//         <div className ="header">
+//         <h2>Todo List: MVP </h2>
+//         <TodoForm addTask ={this.addTask} />
 
-        </div>
+//         </div>
 
-        <div className ="Task-list">
-          {this.state.tasks.map(task => (
-            <task
-            key ={task.id}
-            task ={task}
-            onClick ={(e)=> this.toggleItem(e, task.id)}
-            />
+//         <div className ="Task-list">
+//           {this.state.tasks.map(task => (
+//             <task
+//             key ={task.id}
+//             task ={task}
+//             onClick ={(e)=> this.toggleItem(e, task.id)}
+//             />
 
-          ))}
+//           ))}
 
-          <button className = "clear-btn" onClick ={this.cleartask}>
-            clear Completed
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+//           <button className = "clear-btn" onClick ={this.cleartask}>
+//             clear Completed
+//           </button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+//const rootElement = document.getElementById("root");
+//ReactDOM.render(<App />, rootElement);
 
 export default App;
