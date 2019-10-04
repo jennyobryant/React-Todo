e10725291c8a7bc7d19e552fe5181da39cb628f2
jenny obryant <jennyobryant@protonmail.com>
2019-10-03 20:01:54 -0600

    works in useState hook, but I will work on class component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import "./styles.css";
-import { tsPropertySignature } from '@babel/types';
+
 
 //import TodoForm from "./components/TodoComponents/TodoForm"; 
 //import TodoList from "./components/TodoComponents/TodoList"; 
